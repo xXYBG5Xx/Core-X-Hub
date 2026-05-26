9c5d08266e61e330274ae23e120420aaa6b4ca92
Zeus <xxybg5xx@gmail.com>
2026-05-26 11:02:48 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,1 +1,8 @@
+-- الرابط الأساسي للمستودع الخاص بك (استخدمنا هنا روابط Raw مباشرة لملفاتك)
+local baseUrl = "https://raw.githubusercontent.com/xXYBG5Xx/RedFox-Zero-Hub/main/"
 
+-- 1. تحميل ملف الأوامر والوظائف وتجهيزه في الذاكرة
+_G.RedFoxCommands = loadstring(game:HttpGet(baseUrl .. "commands.lua"))()
+
+-- 2. تشغيل ملف الواجهة (UI) واظهاره على الشاشة
+loadstring(game:HttpGet(baseUrl .. "ui.lua"))()
